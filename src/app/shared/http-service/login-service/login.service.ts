39ed8da2de96afdaa1b268c61941e184c91acc1a
Matheus Armelin <55255174+armelin2001@users.noreply.github.com>
2023-04-13 18:58:37 -0300

    chore: fazendo integração com o back end

---
+++ src/app/shared/http-service/login-service/login.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { LoginDto } from '../../models/login-dto';
+import { environment } from 'src/enviroments/environment';
+
+@Injectable()
+export class LoginService {
+  urlbase = environment.host;
+
+  constructor(private http: HttpClient) { }
+
+  realizaLogin(loginDto: LoginDto): Observable<any>{
+    return this.http.post(`${this.urlbase}/login`, loginDto);
+  }
+
+}
