ea3ce6f3b78e455ccb939f3a6d01d0412e5da06a
Matheus Armelin <55255174+armelin2001@users.noreply.github.com>
2023-04-12 21:37:02 -0300

    feat: criando a tela base de login, criando estrutura base de pastas do projeto e adicionando imgs que serãooo usadas no projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './features/login/login.component';
+import { MaterialModule } from 'src/material.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
