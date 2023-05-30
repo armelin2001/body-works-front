9f39a17f4215bf7ac1e551bf980495617a812044
Matheus Armelin <55255174+armelin2001@users.noreply.github.com>
2023-05-30 12:20:05 -0300

    feat: adicionando local storage ao projeto

---
+++ src/app/models/usuario-dto.ts
@@ -0,0 +1,10 @@
+export interface UsuarioRetornoDTO {
+    id: string;
+    nome: string;
+    cpf: string;
+    genero: string;
+    email: string;
+    senha: string;
+    dataNascimento: string;
+    altura: string;
+}
