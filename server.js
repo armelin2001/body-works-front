b1d8d63e94dffcaa01c4972d6658496c16c8267c
Matheus Armelin <55255174+armelin2001@users.noreply.github.com>
2023-11-04 23:31:42 -0300

    chore: passando nome da aplicação

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/${appName}'));
+app.use(express.static(__dirname + '/dist/body-works-front'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
-'/dist/${appName}/index.html'));});
+'/dist/body-works-front/index.html'));});
 app.listen(process.env.PORT || 8080);
