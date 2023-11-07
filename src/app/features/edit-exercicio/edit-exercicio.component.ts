5c9f5abd8506ae7b4361545207d0c0f44446ac33
Matheus Armelin <55255174+armelin2001@users.noreply.github.com>
2023-11-06 22:35:11 -0300

    chore: alterações de estilo e patch value de exercicio

--- src/app/features/edit-exercicio/edit-exercicio.component.ts
+++ src/app/features/edit-exercicio/edit-exercicio.component.ts
@@ -6,19 +6,22 @@ import {
   Validators,
 } from '@angular/forms';
 import { ExercicioService } from 'src/app/shared/http-service/exercicio-service/exercicio.service';
-import { IExercicioDTO, TIPO_EXERCICIO, NIVEL_DIFICULDADE } from 'src/app/shared/models/exercicio.dto';
+import {
+  IExercicioDTO,
+  TIPO_EXERCICIO,
+  NIVEL_DIFICULDADE,
+} from 'src/app/shared/models/exercicio.dto';
 import { Location } from '@angular/common';
 import { ActivatedRoute, Router } from '@angular/router';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { EquipamentoService } from 'src/app/shared/http-service/equipamento-service/equipamento.service';
 
-
 @Component({
   selector: 'app-edit-exercicio',
   templateUrl: './edit-exercicio.component.html',
-  styleUrls: ['./edit-exercicio.component.scss']
+  styleUrls: ['./edit-exercicio.component.scss'],
 })
-export class EditExercicioComponent implements OnInit{
+export class EditExercicioComponent implements OnInit {
   formularioExercicio: FormGroup;
   equipamentos: any[] = [];
   id: string = '';
@@ -26,13 +29,12 @@ export class EditExercicioComponent implements OnInit{
   dificuldadesExercicio = NIVEL_DIFICULDADE;
 
   constructor(
-    private location: Location,
     private formBuilder: FormBuilder,
     private exercicioService: ExercicioService,
     private snack: MatSnackBar,
     private router: Router,
     private activatedRoute: ActivatedRoute,
-    private equipamentoService: EquipamentoService,
+    private equipamentoService: EquipamentoService
   ) {
     this.formularioExercicio = this.formBuilder.group({
       nome: ['', [Validators.required]],
@@ -43,7 +45,6 @@ export class EditExercicioComponent implements OnInit{
       musculosTrabalhados: ['', [Validators.required]],
     });
   }
-
 
   submitFormExercicio() {
     const exercicio = this.getExercicio();
@@ -57,27 +58,28 @@ export class EditExercicioComponent implements OnInit{
 
   ngOnInit(): void {
     this.id = this.activatedRoute.snapshot.params['id'];
-    this.exercicioService.obterExercicioPorId(this.id).subscribe(
-      (res) => {
-        this.formularioExercicio.patchValue({
-          nome: res.nome,
-          tipoExercicio: res.tipo,
-          equipamentoNecessario: res.equipamentoNecessario,
-          nivelDificuldade: res.nivelDificuldade,
-          videoDemonstrativo: res.videoDemonstrativo,
-          musculosTrabalhados: res.musculosTrabalhados,
-        });
-      },
-      (err) => {
-        this.snack.open(err.error.message, 'OK', {
-          duration: 3000,
-          horizontalPosition: 'center',
-          verticalPosition: 'top',
-        });
-      }
-    );
-    this.equipamentoService.obterTodosEquipamentos().subscribe(data => {
+    this.equipamentoService.obterTodosEquipamentos().subscribe((data) => {
       this.equipamentos = data.dados;
+      this.exercicioService.obterExercicioPorId(this.id).subscribe(
+        (res) => {
+          this.formularioExercicio.patchValue({
+            nome: res.nome,
+            tipoExercicio: res.tipoExercicio,
+            equipamentoNecessario: res.equipamentoNecessario,
+            nivelDificuldade: res.nivelDificuldade,
+            videoDemonstrativo: res.videoDemonstrativo,
+            musculosTrabalhados: res.musculosTrabalhados,
+          });
+        },
+
+        (err) => {
+          this.snack.open(err.error.message, 'OK', {
+            duration: 3000,
+            horizontalPosition: 'center',
+            verticalPosition: 'top',
+          });
+        }
+      );
     });
   }
 
@@ -93,35 +95,45 @@ export class EditExercicioComponent implements OnInit{
     return this.formularioExercicio.get('nome') as FormControl;
   }
   get tipoExercicio(): FormControl {
-    const tipoExercicio = this.formularioExercicio.get('tipoExercicio') as FormControl;
+    const tipoExercicio = this.formularioExercicio.get(
+      'tipoExercicio'
+    ) as FormControl;
     if (tipoExercicio.value.length < 1) {
       tipoExercicio.setErrors({ tipoExercicioInvalido: true });
     }
     return this.formularioExercicio.get('tipoExercicio') as FormControl;
   }
   get equipamentoNecessario(): FormControl {
-    const equipamentoNecessario = this.formularioExercicio.get('equipamentoNecessario') as FormControl;
+    const equipamentoNecessario = this.formularioExercicio.get(
+      'equipamentoNecessario'
+    ) as FormControl;
     if (equipamentoNecessario.value.length < 5) {
       equipamentoNecessario.setErrors({ equipamentoNecessario: true });
     }
     return this.formularioExercicio.get('equipamentoNecessario') as FormControl;
   }
   get nivelDificuldade(): FormControl {
-    const nivelDificuldade = this.formularioExercicio.get('nivelDificuldade') as FormControl;
+    const nivelDificuldade = this.formularioExercicio.get(
+      'nivelDificuldade'
+    ) as FormControl;
     if (nivelDificuldade.value.length < 1) {
       nivelDificuldade.setErrors({ nivelDificuldade: true });
     }
     return this.formularioExercicio.get('nivelDificuldade') as FormControl;
   }
   get videoDemonstrativo(): FormControl {
-    const videoDemonstrativo = this.formularioExercicio.get('videoDemonstrativo') as FormControl;
+    const videoDemonstrativo = this.formularioExercicio.get(
+      'videoDemonstrativo'
+    ) as FormControl;
     if (videoDemonstrativo.value.length < 10) {
       videoDemonstrativo.setErrors({ videoDemonstrativo: true });
     }
     return this.formularioExercicio.get('videoDemonstrativo') as FormControl;
   }
   get musculosTrabalhados(): FormControl {
-    const musculosTrabalhados = this.formularioExercicio.get('musculosTrabalhados') as FormControl;
+    const musculosTrabalhados = this.formularioExercicio.get(
+      'musculosTrabalhados'
+    ) as FormControl;
     if (musculosTrabalhados.value.length < 5) {
       musculosTrabalhados.setErrors({ musculosTrabalhados: true });
     }
@@ -137,7 +149,7 @@ export class EditExercicioComponent implements OnInit{
       equipamentoNecessario: formularioExercicio.equipamentoNecessario,
       nivelDificuldade: formularioExercicio.nivelDificuldade,
       videoDemonstrativo: formularioExercicio.videoDemonstrativo,
-      musculosTrabalhados: formularioExercicio.musculosTrabalhados
+      musculosTrabalhados: formularioExercicio.musculosTrabalhados,
     };
     return exercicio;
   }
--- src/app/features/listagem-exercicio/listagem-exercicio.component.ts
+++ src/app/features/listagem-exercicio/listagem-exercicio.component.ts
@@ -12,6 +12,7 @@ import { MatSort } from '@angular/material/sort';
 import { MatTable, MatTableDataSource } from '@angular/material/table';
 import { Router } from '@angular/router';
 import * as moment from 'moment';
+import { EquipamentoService } from 'src/app/shared/http-service/equipamento-service/equipamento.service';
 import { ExercicioService } from 'src/app/shared/http-service/exercicio-service/exercicio.service';
 export interface ExercicioListagem {
   id: string;
@@ -34,12 +35,19 @@ const ELEMENT_DATA: ExercicioListagem[] = [
 @Component({
   selector: 'app-listagem-exercicio',
   templateUrl: './listagem-exercicio.component.html',
-  styleUrls: ['./listagem-exercicio.component.scss']
+  styleUrls: ['./listagem-exercicio.component.scss'],
 })
 export class ListagemExercicioComponent implements AfterViewInit {
   listaExercicio: ExercicioListagem[] = [];
 
-  displayedColumns: string[] = ['nome', 'tipoExercicio', 'equipamentoNecessario', 'musculosTrabalhados', 'actions'];
+  displayedColumns: string[] = [
+    'nome',
+    'tipoExercicio',
+    'equipamentoNecessario',
+    'musculosTrabalhados',
+    'actions',
+  ];
+  equipamentos: any[] = [];
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
   dataSource!: MatTableDataSource<ExercicioListagem>;
@@ -48,6 +56,7 @@ export class ListagemExercicioComponent implements AfterViewInit {
   constructor(
     private exercicioSerivice: ExercicioService,
     private router: Router,
+    private equipamentoService: EquipamentoService,
     private snack: MatSnackBar
   ) {
     this.dataSource = new MatTableDataSource(ELEMENT_DATA);
@@ -62,21 +71,26 @@ export class ListagemExercicioComponent implements AfterViewInit {
   carregarListaExercicios() {
     this.listaExercicio.slice(0, this.listaExercicio.length);
     this.dataSource.data = [];
-    this.exercicioSerivice
-      .obterTodosExercicios()
-      .subscribe((res) => {
+    this.equipamentoService.obterTodosEquipamentos().subscribe((data) => {
+      this.equipamentos = data.dados;
+      this.exercicioSerivice.obterTodosExercicios().subscribe((res) => {
+        
         res.dados.forEach((exercicio: any) => {
-            const ExercicioResumido: ExercicioListagem = {
-              id: exercicio.id,
-              nome: exercicio.nome,
-              tipoExercicio: exercicio.tipoExercicio,
-              equipamentoNecessario: exercicio.equipamentoNecessario,
-              musculosTrabalhados: exercicio.musculosTrabalhados
-            };
-            this.listaExercicio.push(ExercicioResumido);
-            this.dataSource.data = this.listaExercicio;
+          const equipamento = this.equipamentos.find((equipamento: any) => {
+            return equipamento.id === exercicio.equipamentoNecessario;
+          });
+          const ExercicioResumido: ExercicioListagem = {
+            id: exercicio.id,
+            nome: exercicio.nome,
+            tipoExercicio: exercicio.tipoExercicio,
+            equipamentoNecessario: equipamento.nome,
+            musculosTrabalhados: exercicio.musculosTrabalhados,
+          };
+          this.listaExercicio.push(ExercicioResumido);
+          this.dataSource.data = this.listaExercicio;
         });
       });
+    });
   }
 
   applyFilter(event: Event) {
