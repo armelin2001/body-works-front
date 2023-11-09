d5f9207b0274a0eb58019aa0e9f5186b973efc5b
Matheus Armelin <55255174+armelin2001@users.noreply.github.com>
2023-11-09 15:23:30 -0300

    fix: traduzindo status da conta

--- src/app/shared/components/menu-lateral/menu-lateral.component.ts
+++ src/app/shared/components/menu-lateral/menu-lateral.component.ts
@@ -16,37 +16,67 @@ export class MenuLateralComponent {
   nomeUsuario: string = '';
   statusPagamento: string = '';
   role: string = '';
+  listaStatusPt: string[] = [
+    'Ativo',
+    'Mensalidade em Atraso',
+    'Conta Suspensa',
+  ];
+  listaStatusEn: string[] = [
+    'Active',
+    'Late Monthly Payment',
+    'Suspended Account',
+  ];
   @ViewChild('sidebarRef', { static: false }) sidebarRef!: ElementRef;
 
   constructor(
     private router: Router,
     private localStorage: LocalstorageService,
-    private translateService: TranslateService,
+    private translateService: TranslateService
   ) {}
 
   ngOnInit() {
     const usuario = this.localStorage.obter('usuario') as any;
     this.nomeUsuario = String(usuario.nome).split(' ')[0];
     this.adm = usuario.adm;
+
     if (!usuario.perfil) {
       this.mostraEditInstrutor = true;
     }
     this.id = usuario.id;
     this.role = usuario.role;
+    this.carregaStatusPagamento();
+  }
+
+  carregaStatusPagamento(lingua?: string) {
+    const usuario = this.localStorage.obter('usuario') as any;
+    let listaStatus: string[] = [];
+    console.log(this.translateService.currentLang);
+    console.log(lingua);
+    if(!this.translateService.currentLang){
+      listaStatus = this.listaStatusPt;
+    }
+    if (lingua === 'en' && this.translateService.currentLang === 'pt-br') {
+      listaStatus = this.listaStatusEn;
+    }
+    if (this.translateService.currentLang === 'pt-br') {
+      listaStatus = this.listaStatusPt;
+    }
+    if (this.translateService.currentLang === 'en') {
+      listaStatus = this.listaStatusEn;
+    }
 
     switch (usuario.statusPagamento) {
       case 'ativo':
-        this.statusPagamento = 'Conta ativa';
+        this.statusPagamento = listaStatus[0];
         break;
       case 'atrasado':
-        this.statusPagamento = 'Mensalidade atrasada';
+        this.statusPagamento = listaStatus[1];
         break;
       case 'cancelado':
-        this.statusPagamento = 'Conta suspensa';
+        this.statusPagamento = listaStatus[2];
         break;
     }
   }
-
   navigateToEditCadastro(): void {
     this.router.navigate(['/edit-cadastro/' + this.id]);
   }
@@ -107,7 +137,7 @@ export class MenuLateralComponent {
   navegaParaHistoricoTreino() {
     this.router.navigate(['usuario-historico']);
   }
-  
+
   toggleSidebar() {
     const sidebarWidth = this.sidebarRef.nativeElement.style.width;
     if (sidebarWidth === '0px' || !sidebarWidth) {
@@ -118,13 +148,12 @@ export class MenuLateralComponent {
   }
 
   trocaLinguaEn() {
-    console.log(this.translateService.currentLang)
     this.translateService.use('en');
+    this.carregaStatusPagamento('en');
   }
 
   trocaLinguaPt() {
-    console.log(this.translateService.currentLang)
     this.translateService.use('pt-br');
+    this.carregaStatusPagamento();
   }
-
 }
